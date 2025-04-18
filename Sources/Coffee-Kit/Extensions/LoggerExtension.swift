f4575e3ad0c21833c462abf7cfbe36c8bd4d4f18
Christoph <christoph_rohde@icloud.com>
2025-04-18 18:56:56 +0200

    Add LoggerExtension to remove boilerplate code and profile performance

---
+++ Sources/Coffee-Kit/Extensions/LoggerExtension.swift
@@ -0,0 +1,36 @@
+//
+//  OSLogExtension.swift
+//  Coffee-Kit
+//
+//  Created by Christoph Rohde on 18.04.25.
+//
+import Foundation
+import OSLog
+
+public extension Logger {
+    func trackTask(called name: String, task: () throws -> Void) {
+        let startTime = Date()
+        let duration: TimeInterval
+
+        do {
+            try task()
+            duration = Date().timeIntervalSince(startTime)
+
+            // split duration into minutes, seconds and milliseconds
+            let minutes = Int(duration) / 60
+            let seconds = Int(duration) % 60
+            let milliseconds = Int((duration - Double(minutes * 60 + seconds)) * 1000)
+
+            self.info("Task '\(name)' successful completed in \(minutes) minutes, \(seconds) seconds and \(milliseconds) milliseconds")
+        } catch {
+            duration = Date().timeIntervalSince(startTime)
+
+            // split duration into minutes, seconds and milliseconds
+            let minutes = Int(duration) / 60
+            let seconds = Int(duration) % 60
+            let milliseconds = Int((duration - Double(minutes * 60 + seconds)) * 1000)
+            self.error("Task '\(name)' failed in \(minutes) minutes, \(seconds) seconds and \(milliseconds) milliseconds")
+            self.error("Error: \(error.localizedDescription)")
+        }
+    }
+}
