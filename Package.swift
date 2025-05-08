51a6be64b1827044109a70ce298a63f2c35335de
Christoph <christoph_rohde@icloud.com>
2025-05-08 20:08:11 +0200

    Set Structured Conccurency expizite

--- Package.swift
+++ Package.swift
@@ -16,11 +16,19 @@ let package = Package(
             targets: ["Coffee-Kit"]
         ),
     ],
+    dependencies: [
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Coffee-Kit"),
+            name: "Coffee-Kit",
+            dependencies: [
+            ],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency"),
+            ]
+        ),
         .testTarget(
             name: "Coffee-KitTests",
             dependencies: ["Coffee-Kit"]
