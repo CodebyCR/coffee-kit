4db1156a3557b44b842af14f4ea0c7621d516d11
Christoph <christoph_rohde@icloud.com>
2025-05-08 23:01:16 +0200

    Add OrderBuilderError enum

---
+++ Sources/Coffee-Kit/DAO/Order/Errors/OrderBuilderError.swift
@@ -0,0 +1,12 @@
+//
+//  OrderBuilderError.swift
+//  Coffee-Kit
+//
+//  Created by Christoph Rohde on 08.05.25.
+//
+
+import Foundation
+
+public enum OrderBuilderError: Error {
+    case emptyShopingCart
+}
