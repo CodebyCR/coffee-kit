8ea49c242fd45c2d51210db52b508313fcbe05f9
Christoph Rohde <christoph_rohde@icloud.com>
2025-12-21 21:10:08 +0100

    Switch from static IP to alias host name

--- Sources/Coffee-Kit/Webservice/DatabaseAPI.swift
+++ Sources/Coffee-Kit/Webservice/DatabaseAPI.swift
@@ -10,18 +10,18 @@ import Foundation
 public struct DatabaseAPI {
     public let baseURL: URL
     public let socketURL: URL
-    private static let localIP = "192.168.178.21"
+    private static let localHostname = "cr-mac.local"
 
     public static var dev: DatabaseAPI {
-        let baseURL = URL(string: "http://\(localIP):8080/test")!
-        let socketURL = URL(string: "ws://\(localIP):8080/test")!
+        let baseURL = URL(string: "http://\(localHostname):8080/test")!
+        let socketURL = URL(string: "ws://\(localHostname):8080/test")!
 
         return DatabaseAPI(baseURL: baseURL, socketURL: socketURL)
     }
 
     public static var productiv: DatabaseAPI {
-        let baseURL = URL(string: "http://\(localIP):8080/prod")!
-        let socketURL = URL(string: "ws://\(localIP):8080/prod")!
+        let baseURL = URL(string: "http://\(localHostname):8080/prod")!
+        let socketURL = URL(string: "ws://\(localHostname):8080/prod")!
 
         return DatabaseAPI(baseURL: baseURL, socketURL: socketURL)
     }
